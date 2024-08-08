5bf40c42a936ba418ed43ff32bff9cbab94a03a8
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:08:21 +0500

    Add files via upload

---
+++ Question 14/014.js
@@ -0,0 +1,5 @@
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+// Make a list that includes at least three people you’d like to invite to dinner.
+// Then use your list to print a message to each person, inviting them to dinner.
+var guestArray = ["bisma", "fatima", "eisha"];
+guestArray.map(function (items) { return console.log("Hello, ".concat(items, " you are invited to party.")); });
---
+++ Question 14/014.ts
@@ -0,0 +1,8 @@
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+// Make a list that includes at least three people you’d like to invite to dinner.
+// Then use your list to print a message to each person, inviting them to dinner.
+
+
+let guestArray: string[] = ["bisma","fatima","eisha"];
+
+guestArray.map((items)=> console.log(`Hello, ${items} you are invited to party.`));
